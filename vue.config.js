ad2ae92165bffe6a69bcd605e5069b3345da16e4
Peter wu <peter.wu@neutec.com.tw>
2024-04-27 12:37:20 +0800

    feat: update6

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath: "./",
-  outputDir: "dist",
+  outputDir: "public",
   indexPath: "index.html",
   transpileDependencies: true,
 });
